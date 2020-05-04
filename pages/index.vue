22760aed86cd04adf32d0ab761cc759adb6c6663
Ronny Cabrera Sinarahua <ronny@mesa247.pe>
2020-05-04 12:25:24 -0500

    chore: add list products

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,68 @@
 <template lang="pug">
-	.container
-		div(v-if="!$store.state.isLoged")
-			logo
-			h1.title CHALLENGE
-			h2.subtitle proyecto test challenge
-			nuxt-link.session(to="/login") Login
-			nuxt-link.btn.btn_green(to="/register") Register
-		div(v-else)
-			logo
-			h1.title CHALLENGE
-			h2.subtitle Mira los Productos
-			nuxt-link.session(to="/companies") Companies
+	fragment
+		.container
+			div(v-if="!$store.state.isLoged")
+				logo
+				h1.title CHALLENGE
+				h2.subtitle proyecto test challenge
+				nuxt-link.session(to="/login") Login
+				nuxt-link.btn.btn_green(to="/register") Register
+			div(v-else)
+				logo
+				h1.title CHALLENGE
+				h2.subtitle Mira los Productos
+				nuxt-link.session(to="/companies") Companies
+					ul( v-if="products&&products.length>0")
+						li( v-for="(item, idx) in products" :key="idx")
+							p {{ item.name }}
+					ul(v-else) {{ 'gika' }}
+
 </template>
+
 <script>
 import Logo from '~/components/Logo.vue'
+import axios from 'axios';
 export default {
-components: {
-	Logo
-}
+	components: {
+		Logo
+	},
+	data(){
+		return{
+			products:[]
+		}
+	},
+	mounted(){
+		let self = this;
+		setTimeout(
+			()=>{
+				self.dataGetProducts();
+			}, 300
+		)
+	},
+	methods: {
+		dataGetProducts(){
+			let self = this;
+			console.log(self.$store.state);
+			if(self.$store.state.user.companyId){
+				axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+				self.$axios.get(process.env.baseUrl+'products').then(res=>{
+					console.log('data',res.data)
+					self.products= res.data;
+					setTimeout(
+					()=>{
+							//self.getProductsUser();
+						}, 200
+					)
+				})
+			}
+		},
+		getProductsUser(){
+			let self = this;
+			self.products = [];
+			console.log('entro');
+
+		},
+	}
 }
 </script>
 
